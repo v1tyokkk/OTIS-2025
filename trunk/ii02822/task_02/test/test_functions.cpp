b61dc4e4b731def34207444c595a6c5a3202298e
Илья Олексюк <brest155304@gmail.com>
2025-11-26 15:14:43 +0300

    [task_02] Did lab2 (#222)
    * добавил файлы
    
    * Add files
    
    * [task_01] Lab1
    
    * изменил файл
    
    * [task_01]LAB1
    
    * изменил файл cpp
    
    * изменил cmakelist
    
    * внес изменения
    
    * внес изменения
    
    * исправили ошибки от copilot
    
    * исправил старый код на новый в readme.md
    
    * внесли изменения
    
    * изменения
    
    * readme.md
    
    * замена readme. md
    
    * изменения
    
    * внесли изменения
    
    * сделал 2 лабораторную работу
    
    * исправили ошибки
    
    * изменил файлы
    
    * внесли изменения
    
    * изменили код
    
    * исправления
    
    * .
    
    * изменения
    
    * исправил код
    
    * изменения
    
    * перенесли cmakelist
    
    * исправили ошибку
    
    * изменения
    
    * Delete trunk/ii02822/task_02/src/CMakeLists.txt
    
    * удалили не нужный cmake
    
    * cmakelists
    
    * изменения
    
    * внесли вправки в cmake
    
    * cmakelist
    
    * изменения
    
    * исправили код
    
    * внесли правки
    
    * добавили скриншот с информацией об покрытии

---
+++ trunk/ii02822/task_02/test/test_functions.cpp
@@ -0,0 +1,23 @@
+﻿#include <gtest/gtest.h>
+#include <cmath>
+#include "functions.h" 
+
+TEST(LinearModelTest, BaseCase) {
+    EXPECT_NEAR(linear(0.25, 0.45, 0.6, 0.1), 0.6 * 0.25 + 0.1 * 0.45, 1e-6);
+}
+
+TEST(LinearModelTest, WithDifferentCoefficients) {
+    EXPECT_NEAR(linear(0.35, 0.55, 1.5, 0.8), 1.5 * 0.35 + 0.8 * 0.55, 1e-6);
+}
+
+TEST(NonlinearModelTest, BaseCase) {
+    double result = non_linear(0.2, 0.1, 0.1, 0.8, 0.6, 0.15, 0.2);
+    double expected = 0.8 * 0.2 - 0.6 * 0.1 * 0.1 + 0.15 * 0.1 + 0.2 * std::sin(0.1);
+    EXPECT_NEAR(result, expected, 1e-6);
+}
+
+TEST(NonlinearModelTest, WithValues) {
+    double result = non_linear(0.6, 0.4, 0.4, 0.4, 0.3, 0.25, 0.15);
+    double expected = 0.4 * 0.6 - 0.3 * 0.4 * 0.4 + 0.25 * 0.4 + 0.15 * std::sin(0.4);
+    EXPECT_NEAR(result, expected, 1e-6);
+}
