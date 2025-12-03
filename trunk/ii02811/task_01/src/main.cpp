5c49c2aefd15aa52f4543ea40581c171b86731cb
GDKostettt <ii002810@g.bstu.by>
2025-12-03 15:33:24 +0300

    Completed task 2 (#259)

--- trunk/ii02811/task_01/src/main.cpp
+++ trunk/ii02811/task_01/src/main.cpp
@@ -1,17 +1,8 @@
+#include "main.h"
 #include <iostream>
 #include  <cmath>
 
 using namespace std;
-
-double x(double  y, double  u, double  a, double  b)
-{
-    return  a  *  y  +  b  *  u;
-}
-
-double x1(double y, double  u, double  a, double  b, double  c, double  d, double yx)
-{
-    return  a  *  y  -  b  *  y  *  yx  +  c  *  u  +  d  *  sin(u);
-}
 
 int main(){
     
