1da43fc8590d1eca7040cf3d153c0be30251fc40
Glouston <64912220+Glouston@users.noreply.github.com>
2025-12-23 10:21:10 +0300

    lab2 complety (#315)

---
+++ trunk/ii02724/task_02/src/Nonlinear.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+#include "Nonlinear.h"
+using namespace std;
+
+void Nonlinear(const Constant &c, int steps_count, double& current_temp, double current_heat, double next_temp, double previous_temp, double previous_heat) {
+
+
+    for (int step = 1; step <= 1; step++)
+    {
+
+        next_temp = c.param_a * current_temp -
+            c.param_b * previous_temp * previous_temp +
+            c.param_c * current_heat +
+            c.param_d * sin(previous_heat);
+
+
+        previous_temp = current_temp;
+        current_temp = next_temp;
+
+
+        previous_heat = current_heat;
+
+        if (step < steps_count) {
+            cout << "Provide heat input for step " << step + 1 << ": ";
+            cin >> current_heat;
+        }
+    }
+}
---
+++ trunk/ii02724/task_02/src/otis2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <math.h>
+#include "Linear.h"
+#include "Nonlinear.h"
+using namespace std;
+
+int main()
+{
+
+    double current_temp = 0;
+    double previous_temp = 0;
+    double next_temp = 0;
+    double current_heat = 0;
+    double previous_heat = 0;
+    double param_a = 0;
+    double param_b = 0;
+    double param_c = 0;
+    double param_d = 0;
+    int steps_count = 0;
+
+
+    cout << "Input initial temperature and heat (separated by space): ";
+    cin >> current_temp >> current_heat;
+
+    cout << "Input coefficients a, b, c, d: ";
+    cin >> param_a >> param_b >> param_c >> param_d;
+
+    cout << "Input simulation steps count: ";
+    cin >> steps_count;
+
+
+    double initial_temp = current_temp;
+
+    Linear(param_a, param_b, steps_count, current_temp, current_heat);
+
+    current_temp = initial_temp;
+    previous_temp = 0.0;
+    previous_heat = current_heat;
+    Constant c;
+    c.param_a = 2.3;
+    c.param_b = 2.3;
+    c.param_c = 2.3;
+    c.param_d = 2.3;
+    Nonlinear(c,steps_count,current_temp, current_heat, next_temp, previous_temp, previous_heat);
+    cout << "\nSimulation completed!" << endl;
+    return 0;
+}
